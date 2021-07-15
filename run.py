2e253d11eb6c2fd9845f11535ed7a0fe53b38b6c
Catrionamcd <catrionamcd@gmail.com>
2021-07-15 16:13:10 +0000

    Create introduction to the game & function to start game

--- run.py
+++ run.py
@@ -1,1 +1,40 @@
-print("Adventures of Alice")
+# A text base adventure game called Adventures of Alice
+# Author : Catriona McDonnell
+
+
+"""
+Introduction to the game and game name
+"""
+print()
+print("**************************************************")
+print()
+print("Aventure game that brings you through a Wonderland")
+print("with Alice. Meet fantastic creatures along the way")
+print("and help restore balance to Wonderland by helping")
+print("Alice to slay the Jabberwocky!")
+
+print()
+print("    ***************************************")
+print("    *                                     *")
+print("    *             Welcome to              *")
+print("    *                the                  *")
+print("    *         Adventures of Alice         *")
+print("    *                                     *")
+print("    *                                     *")
+print("    ***************************************")
+
+
+def game_start():
+    """
+    Start of game 
+    """
+    print()
+    player = input("Enter your name here to start the adventure:\n")
+    if player == "":
+        print("To play the game you must enter a name !!")
+    else:
+        print(f"Welcome {player} to the world of Alice")
+
+game_start()
+    
+    
